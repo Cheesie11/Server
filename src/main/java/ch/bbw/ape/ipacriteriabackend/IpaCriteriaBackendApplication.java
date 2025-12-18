12fadd1b04885616e25daf1ddbb6a1d57b89c53e
Erjon Thaqi <e3cthe@e3ag.net>
2025-12-18 12:00:11 +0100

    initial commit

---
+++ src/main/java/ch/bbw/ape/ipacriteriabackend/IpaCriteriaBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.ape.ipacriteriabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpaCriteriaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpaCriteriaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/ape/ipacriteriabackend/IpaCriteriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ape.ipacriteriabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpaCriteriaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
